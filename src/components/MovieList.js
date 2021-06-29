97fe2c4f743b4c8c1d3d0d59638baaf95d777fcb
Carter Walsh <Cwalsh616@gmail.com>
2021-06-28 20:17:16 -0500

    cleaned up MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -26,12 +26,3 @@ const MovieList = ({ list, onDelete }) => {
 }
 
 export default MovieList;
-
-// {list.map((item) => (
-//     <div key={item.id.toString()} >      
-//         <li onClick={toggleClass} className={activeClass ? "checked" : null}>
-//             {item.name}
-//         </li>
-//         <button onClick={(event) => onDelete(event.target.parentElement.childNodes[0].outerText)} className="delete-button">X</button>
-//     </div>
-// ))}
